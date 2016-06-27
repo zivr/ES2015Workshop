e2aa7f489ff0ce90a7e4ba6495bdce6994dce852
502549131 <ziv.rosenzweig@ge.com>
2016-06-27 15:45:45 +0300

    convert sliderView to es2015 class + arrow function

--- js/sliderView.js
+++ js/sliderView.js
@@ -1,49 +1,49 @@
-var SliderView = function (board , $rootElement) {
-    this._board = board;
-    this._$rootEl = $rootElement;
+class SliderView {
+    constructor(board , $rootElement) {
+        this._board = board;
+        this._$rootEl = $rootElement;
 
-    $(this._board).on(Board.EVENTS.SWAP, this.onSwapSquares.bind(this));
-    $(this._board).on(Board.EVENTS.SHUFFEL, this.resetUI.bind(this));
-};
+        $(this._board).on(Board.EVENTS.SWAP, this.onSwapSquares.bind(this));
+        $(this._board).on(Board.EVENTS.SHUFFEL, this.resetUI.bind(this));
+    }
 
-SliderView.prototype.resetUI = function() {
-    var rows = this._board.rowsCount;
-    var self = this;
+    resetUI() {
+        const rows = this._board.rowsCount;
 
-    this._$rootEl.addClass('board board--'+rows+'-in-row');
+        this._$rootEl.addClass('board board--'+rows+'-in-row');
 
-    this._board.squares.forEach(function (square) {
-        var $squareEl = $('<div class="board__cell">' + (square.value || '&nbsp;') + '</div>');
-        $squareEl.data('square', square);
-        if (square.isBlank) {
-            $squareEl.addClass('board__empty-cell');
-        }
-        else if (square.value % 2) {
-            $squareEl.addClass('board__cell--odd');
-        }
-        else {
-            $squareEl.addClass('board__cell--even');
-        }
-        self._$rootEl.append($squareEl);
-        if (!square.isBlank) {
-            $squareEl.click(self.onSquareClicked.bind(self));
-        }
-    });
-};
+        this._board.squares.forEach((square) => {
+            const $squareEl = $('<div class="board__cell">' + (square.value || '&nbsp;') + '</div>');
+            $squareEl.data('square', square);
+            if (square.isBlank) {
+                $squareEl.addClass('board__empty-cell');
+            }
+            else if (square.value % 2) {
+                $squareEl.addClass('board__cell--odd');
+            }
+            else {
+                $squareEl.addClass('board__cell--even');
+            }
+            this._$rootEl.append($squareEl);
+            if (!square.isBlank) {
+                $squareEl.click(this.onSquareClicked.bind(this));
+            }
+        });
+    }
 
+    onSquareClicked(e) {
+        const $clickedSquare = $(e.currentTarget);
+        this._board.moveSquare($clickedSquare.data('square'));
+        $('.js-moves-countr').text(this._board.moves);
+    }
 
-SliderView.prototype.onSquareClicked = function(e) {
-    var $clickedSquare = $(e.currentTarget);
-    this._board.moveSquare($clickedSquare.data('square'));
-    $('.js-moves-countr').text(this._board.moves);
-};
-
-SliderView.prototype.onSwapSquares = function(e , data) {
-    var $elA = this._$rootEl.find('div:nth-child('+(data.posA+1)+')');
-    var $elB = this._$rootEl.find('div:nth-child('+(data.posB+1)+')');
-    var $tmpEl = $('<span>').hide();
-    $elA.before($tmpEl);
-    $elB.before($elA);
-    $tmpEl.replaceWith($elB);
-};
+    onSwapSquares(e , data) {
+        const $elA = this._$rootEl.find('div:nth-child('+(data.posA+1)+')');
+        const $elB = this._$rootEl.find('div:nth-child('+(data.posB+1)+')');
+        const $tmpEl = $('<span>').hide();
+        $elA.before($tmpEl);
+        $elB.before($elA);
+        $tmpEl.replaceWith($elB);
+    }
+}
 
