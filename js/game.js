e67500dbc7ee1dcd5f5230dcc0187eb042fd6758
502549131 <ziv.rosenzweig@ge.com>
2016-06-27 15:10:42 +0300

    convert game to es2015 class

--- js/game.js
+++ js/game.js
@@ -1,22 +1,24 @@
-var Game = function (elementsInRow) {
-    this._board = new Board(elementsInRow);
-    this._view = new SliderView(this._board , $('.js-board'));
-    $(this._board).on(Board.EVENTS.WIN, this._onGameWon.bind(this));
-};
+class Game {
+    constructor(elementsInRow) {
+        this._board = new Board(elementsInRow);
+        this._view = new SliderView(this._board , $('.js-board'));
+        $(this._board).on(Board.EVENTS.WIN, this.onGameWon.bind(this));
+    }
 
-Game.startGame = function(elementsInRow) {
-    var g = new Game(elementsInRow);
-    g.shuffle();
-    return g;
-};
+    static startGame(elementsInRow) {
+        const g = new Game(elementsInRow);
+        g.shuffle();
+        return g;
+    }
 
-Game.prototype.shuffle = function() {
-    this._board.shuffle();
-};
+    shuffle() {
+        this._board.shuffle();
+    }
 
-Game.prototype._onGameWon = function() {
-    alert('congratulation');
-};
+    onGameWon() {
+        alert('congratulation');
+    }
+}
 
 
 
