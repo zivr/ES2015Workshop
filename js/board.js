6d449f23a06f5777d2cefac3778fa49404aeeba2
502549131 <ziv.rosenzweig@ge.com>
2016-06-27 15:19:31 +0300

    convert board to es2015 class + use let, const

--- js/board.js
+++ js/board.js
@@ -1,93 +1,89 @@
-var Board = function(elementsInRow) {
-    this._elementsInRow = elementsInRow;
-    this.squares = new SquareCollection();
-    this._movesCount = 0;
-};
+class Board {
+    constructor(elementsInRow) {
+        this._elementsInRow = elementsInRow;
+        this.squares = new SquareCollection();
+        this._movesCount = 0;
+    }
 
-Board.EVENTS = {
-    SHUFFEL: 'SHUFFEL',
-    SWAP: 'SWAP',
-    WIN: 'WIN'
-};
+    static get EVENTS() {
+        return {
+            SHUFFEL: 'SHUFFEL',
+            SWAP: 'SWAP',
+            WIN: 'WIN'
+        };
+    }
 
-Object.defineProperty(Board.prototype, "rowsCount", {
-    get: function() {
+    get rowsCount() {
         return this._elementsInRow;
     }
-});
 
-Object.defineProperty(Board.prototype, "colsCount", {
-    get: function() {
+    get colsCount() {
         return this._elementsInRow;
     }
-});
 
-Object.defineProperty(Board.prototype, "moves", {
-    get: function() {
+    get moves() {
         return this._movesCount;
     }
-});
 
-//Create a new location for each square on the board.
-Board.prototype.shuffle = function() {
-    var amountOfSquare = this.colsCount * this.rowsCount;
-    var squaresNum = [];
-    var i;
+    shuffle() {
+        const amountOfSquare = this.colsCount * this.rowsCount;
 
-    for (i = 0; i < amountOfSquare; i++) {
-        squaresNum[i] = i + 1;
+        let squaresNum = [];
+        for (let i = 0; i < amountOfSquare; i++) {
+            squaresNum[i] = i + 1;
+        }
+        squaresNum[amountOfSquare - 1] = null;
+
+        this.squares.clean();
+        for (let i = 1; i <= amountOfSquare; i++) {
+            const num = Math.floor(Math.random() * squaresNum.length);
+            this.squares.add(new Square(squaresNum[num]));
+            squaresNum.splice(num, 1);
+        }
+        if (!this._isSolvable()){
+            this.shuffle();
+            return;
+        }
+        $(this).trigger(Board.EVENTS.SHUFFEL);
     }
-    squaresNum[amountOfSquare - 1] = null;
 
-    this.squares.clean();
-    for (i = 1; i <= amountOfSquare; i++) {
-        var num = Math.floor(Math.random() * squaresNum.length);
-        this.squares.add(new Square(squaresNum[num]));
-        squaresNum.splice(num, 1);
+    moveSquare(square) {
+        var blankSquare = this.squares.getBlankSquare();
+        if (this.squares.isNeighbors(square, blankSquare)) {
+            this.squares.swap(blankSquare, square);
+            $(this).trigger(Board.EVENTS.SWAP, {
+                squareA: square,
+                posA: this.squares.indexOf(square),
+                squareB: blankSquare,
+                posB: this.squares.indexOf(blankSquare)
+            });
+            this._movesCount++;
+            this._checkIsWinning();
+        }
     }
-    if (!this._isSolvable()){
-        this.shuffle();
-        return;
-    }
-    $(this).trigger(Board.EVENTS.SHUFFEL);
-};
 
-Board.prototype.moveSquare = function(square) {
-    var blankSquare = this.squares.getBlankSquare();
-    if (this.squares.isNeighbors(square, blankSquare)) {
-        this.squares.swap(blankSquare, square);
-        $(this).trigger(Board.EVENTS.SWAP, {
-            squareA: square,
-            posA: this.squares.indexOf(square),
-            squareB: blankSquare,
-            posB: this.squares.indexOf(blankSquare)
+    _checkIsWinning() {
+        var isWinning = this.squares.every(function(square, index) {
+            return square.isBlank || square.value === (index + 1);
         });
-        this._movesCount++;
-        this._checkIsWinning();
+        if (isWinning) {
+            $(this).trigger(Board.EVENTS.WIN);
+        }
     }
-};
 
-Board.prototype._checkIsWinning = function() {
-    var isWinning = this.squares.every(function(square, index) {
-        return square.isBlank || square.value === (index + 1);
-    });
-    if (isWinning) {
-        $(this).trigger(Board.EVENTS.WIN);
-    }
-};
-
-Board.prototype._isSolvable = function() {
-    var inversions = 0;
-    var squares = this.squares;
-    squares.every(function(squareI, idx) {
-        squares.every(function(squareJ) {
-            if (squareJ.isBlank || squareI.value > squareJ.value){
+    _isSolvable() {
+        var inversions = 0;
+        var squares = this.squares;
+        squares.every(function(squareI, idx) {
+            squares.every(function(squareJ) {
+                if (squareJ.isBlank || squareI.value > squareJ.value){
+                    inversions++;
+                }
+            });
+            if (squareI.isBlank && idx % 2 === 1) {
                 inversions++;
             }
         });
-        if (squareI.isBlank && idx % 2 === 1) {
-            inversions++;
-        }
-    });
-    return (inversions % 2 == 0);
-};
+        return (inversions % 2 == 0);
+    };
+}
