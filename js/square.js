7cbceeb7cb89388f9e1432efdcc37d5230955485
502549131 <ziv.rosenzweig@ge.com>
2016-06-27 15:53:56 +0300

    convert Square & SquareCollection to es2015 class & let & const

--- js/square.js
+++ js/square.js
@@ -1,72 +1,73 @@
-var Square = function(value) {
-    this._val = value;
-};
+class Square {
+    constructor(value) {
+        this._val = value;
+    }
 
-Object.defineProperty(Square.prototype, "value", {
-    get: function() {
+    get value() {
         return this._val;
     }
-});
 
-Object.defineProperty(Square.prototype, "isBlank", {
-    get: function() {
+    get isBlank() {
         return this._val === null || isNaN(this._val);
     }
-});
+}
 
 
-var SquareCollection = function() {
-    this.squares = [];
-};
 
-SquareCollection.prototype.add = function(square) {
-    this.squares.push(square);
-};
+class SquareCollection{
+    constructor() {
+        this.squares = [];
+    }
 
-SquareCollection.prototype.clean = function() {
-    this.squares = [];
-};
+    add(square) {
+        this.squares.push(square);
+    }
 
-SquareCollection.prototype.getBlankSquare = function() {
-    //We don't use find because it is not supported by IE
-    var i = 0, len = this.squares.length;
-    var square;
-    do {
-        square = this.squares[i++];
-    } while (i < len && !square.isBlank);
-    return square;
-};
+    clean() {
+        this.squares = [];
+    }
 
-SquareCollection.prototype.indexOf = function(square) {
-    return this.squares.indexOf(square);
-};
+    getBlankSquare() {
+        //We don't use find because it is not supported by IE
+        const len = this.squares.length;
+        let i = 0, square;
+        do {
+            square = this.squares[i++];
+        } while (i < len && !square.isBlank);
+        return square;
+    }
 
-SquareCollection.prototype.swap = function(squareA, squareB) {
-    var squareAPos = this.indexOf(squareA);
-    var squareBPos = this.indexOf(squareB);
-    this.squares[squareAPos] = squareB;
-    this.squares[squareBPos] = squareA;
-};
+    indexOf(square) {
+        return this.squares.indexOf(square);
+    }
 
-SquareCollection.prototype._convertTo2D = function(square) {
-    var numOfCols = Math.sqrt(this.squares.length);
-    var index = this.squares.indexOf(square);
-    return { x: Math.floor(index / numOfCols), y: index % numOfCols }
-};
+    swap(squareA, squareB) {
+        const squareAPos = this.indexOf(squareA);
+        const squareBPos = this.indexOf(squareB);
+        this.squares[squareAPos] = squareB;
+        this.squares[squareBPos] = squareA;
+    }
 
-SquareCollection.prototype.isNeighbors = function(squareA, squareB) {
-    var squareAPos = this._convertTo2D(squareA);
-    var squareBPos = this._convertTo2D(squareB);
+    _convertTo2D(square) {
+        const numOfCols = Math.sqrt(this.squares.length);
+        const index = this.squares.indexOf(square);
+        return { x: Math.floor(index / numOfCols), y: index % numOfCols }
+    }
 
-    return ((squareAPos.x === squareBPos.x && Math.abs(squareAPos.y - squareBPos.y) === 1) ||
-    (squareAPos.y === squareBPos.y && Math.abs(squareAPos.x - squareBPos.x) === 1));
-};
+    isNeighbors(squareA, squareB) {
+        const squareAPos = this._convertTo2D(squareA);
+        const squareBPos = this._convertTo2D(squareB);
 
-SquareCollection.prototype.forEach = function (callback) {
-    this.squares.forEach(callback);
-};
+        return ((squareAPos.x === squareBPos.x && Math.abs(squareAPos.y - squareBPos.y) === 1) ||
+        (squareAPos.y === squareBPos.y && Math.abs(squareAPos.x - squareBPos.x) === 1));
+    }
 
-SquareCollection.prototype.every = function (callback) {
-    return this.squares.every(callback);
-};
+    forEach(callback) {
+        this.squares.forEach(callback);
+    }
+
+    every(callback) {
+        return this.squares.every(callback);
+    }
+}
 
